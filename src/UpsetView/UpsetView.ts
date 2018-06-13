d545244415d81948de1960e80b15657393472a3d
Kiran Gadhave <kirangadhave2@gmail.com>
2018-06-12 23:25:33 -0600

    Added and styled usedSetHeaders and connectors

--- src/UpsetView/UpsetView.ts
+++ src/UpsetView/UpsetView.ts
@@ -1,3 +1,4 @@
+import { d3Selection } from "./../type_declarations/types";
 /*
  * @Author: Kiran Gadhave 
  * @Date: 2018-06-03 14:36:32 
@@ -12,29 +13,164 @@ import { ViewBase } from "provenance_mvvm_framework";
 import html from "./upset.view.html";
 import params from "./ui_params";
 export class UpsetView extends ViewBase {
+  headerVis: d3Selection;
+  bodyVis: d3Selection;
+  headerSVG: d3Selection;
+  usedSetsHeaderGroup: d3Selection;
+
   constructor(root: HTMLElement) {
     super(root);
   }
 
   create() {
     d3.select(this.Root).html(html);
+    this.headerVis = d3
+      .select(this.Root)
+      .select("#header-vis")
+      .style("padding", "5px 0px");
+    this.bodyVis = d3.select(this.Root).select("#body-vis");
+
+    let width = (this.headerVis.node() as HTMLElement).getBoundingClientRect()
+      .width;
+
+    this.headerSVG = this.headerVis
+      .append("svg")
+      .style("width", width)
+      .style("height", params.header_height);
+    this.usedSetsHeaderGroup = this.headerSVG
+      .append("g")
+      .attr("class", "usedSetGroup")
+      .attr("transform", `translate(0,${params.padding})`);
   }
 
   update(data: Data) {
-    console.log(data);
+    this.updateUsedSetHeader(
+      data.usedSets,
+      d3.max(data.sets.map(d => d.setSize))
+    );
+    this.updateUsedSetConnectors(
+      data.usedSets,
+      d3.max(data.sets.map(d => d.setSize))
+    );
   }
+
+  private updateUsedSetConnectors(data: Set[], maxSetSize: number) {
+    let usedSetConnectorGroup = this.usedSetsHeaderGroup
+      .append("g")
+      .attr("class", "usedSetsConnector")
+      .attr("transform", `translate(0, ${params.used_set_header_height})`);
+
+    let connectors = usedSetConnectorGroup
+      .selectAll(".usedSetConnector")
+      .data(data);
+
+    connectors.exit().remove();
+
+    let connectorsEnter = connectors
+      .enter()
+      .append("g")
+      .attr("class", "usedSetConnector")
+      .attr("transform", (d, i) => {
+        return `translate(${params.col_width * i}, 0)`;
+      })
+      .on("mouseover", this.mouseover.bind(this))
+      .on("mouseout", this.mouseout.bind(this))
+      .on("click", this.click.bind(this));
+
+    connectorsEnter
+      .append("rect")
+      .attr("class", (d, i) => {
+        return `connector setSizeBackground ${d.id}`;
+      })
+      .attr("width", params.col_width)
+      .attr("height", params.connector_height)
+      .attr("transform", "skewX(45)");
+
+    connectorsEnter
+      .append("text")
+      .text((d, i) => {
+        return d.elementName;
+      })
+      .style("text-anchor", "end");
+
+    let textHeight = (connectorsEnter.select("text").node() as any).getBBox()
+      .height;
+
+    connectorsEnter
+      .selectAll("text")
+      .attr(
+        "transform",
+        `translate(${params.connector_height / Math.sqrt(2) +
+          params.col_width / Math.sqrt(2) +
+          textHeight / 2}, ${params.connector_height})rotate(45)`
+      );
+  }
+
+  private updateUsedSetHeader(data: Set[], maxSetSize: number) {
+    let usedSetsGroup = this.usedSetsHeaderGroup
+      .append("g")
+      .attr("class", "usedSets");
+
+    let usedSets = usedSetsGroup.selectAll(".usedSet").data(data);
+
+    usedSets.exit().remove();
+
+    let usedSetsEnter = usedSets
+      .enter()
+      .append("g")
+      .attr("class", "usedSet")
+      .on("mouseover", this.mouseover.bind(this))
+      .on("mouseout", this.mouseout.bind(this))
+      .on("click", this.click.bind(this));
+    usedSetsEnter
+      .append("rect")
+      .attr("class", (d, i) => {
+        return `setSizeBackground ${d.id}`;
+      })
+      .attr("width", params.col_width)
+      .attr("height", params.used_set_header_height)
+      .attr("transform", (d, i) => {
+        return `translate(${params.col_width * i}, 0)`;
+      });
+
+    usedSetsEnter
+      .append("rect")
+      .attr("class", "setSizeForeground")
+      .attr("width", params.col_width)
+      .attr("height", (d, i) => {
+        return setSizeScale(
+          maxSetSize,
+          params.used_set_header_height,
+          d.setSize
+        );
+      })
+      .attr("transform", (d, i) => {
+        return `translate(${params.col_width *
+          i}, ${params.used_set_header_height -
+          setSizeScale(maxSetSize, params.used_set_header_height, d.setSize)})`;
+      });
+  }
+
+  private mouseover(data: Set, idx: number) {
+    this.headerSVG.selectAll(`.${data.id}`).style("fill", "rgb(254, 217, 166)");
+  }
+
+  private mouseout(data: Set, idx: number) {
+    this.headerSVG.selectAll(`.${data.id}`).style("fill", "#ddd");
+  }
+
+  private click(data: Set, idx: number) {}
 }
 
-function setSizeScale(data: Set[], size: number): number {
+function setSizeScale(
+  maxDomain: number,
+  maxRange: number,
+  size: number
+): number {
   let scale = d3
     .scaleLinear()
-    .domain([
-      0,
-      d3.max(data, (d: Set) => {
-        return d.setSize;
-      })
-    ])
+    .domain([0, maxDomain])
     .nice()
-    .range([0, this.usedSetHeight]);
+    .range([0, maxRange]);
   return scale(size);
 }
--- src/UpsetView/ui_params.ts
+++ src/UpsetView/ui_params.ts
@@ -1,6 +1,15 @@
 let params = {
-  total_height: "100vh",
-  svg_header_height: 70
+  padding: 7,
+
+  col_width: 30,
+
+  used_set_header_height: 70,
+  connector_height: 80,
+  get header_height() {
+    return (
+      this.used_set_header_height + this.connector_height + this.padding * 2
+    );
+  }
 };
 
 export default params;
