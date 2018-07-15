40fa24b16833b1a3ae24679581eb06cc5f40f5b5
Kiran Gadhave <kirangadhave2@gmail.com>
2018-07-15 13:32:22 -0600

    Added a view for testing embed

--- src/EmbedGenView/EmbedGenView.ts
+++ src/EmbedGenView/EmbedGenView.ts
@@ -1,9 +1,14 @@
 import { d3Selection } from "./../type_declarations/types";
+import * as d3 from "d3";
 import html from "./embedgen.view.html";
-export function EmbedGenView(base: d3Selection) {
-  base.html(base.html() + html);
+import "./styles.scss";
 
+export function EmbedGenView(base: d3Selection) {
+  let embedBox = d3.select(".embeded-view").append("iframe");
+  embedBox.attr("width", "100%").attr("height", "800px");
+  base.html(base.html() + html);
   addShowCloseEvents(base);
+  addInsertIFrameEvent(base, embedBox);
 }
 
 function addShowCloseEvents(base: d3Selection) {
@@ -16,4 +21,8 @@ function addShowCloseEvents(base: d3Selection) {
   base.select(".modal-background").on("click", () => {
     base.select(".modal").classed("is-active", false);
   });
+}
+
+function addInsertIFrameEvent(base: d3Selection, iframe: d3Selection) {
+  base.select(".copy-button").on("click", () => {});
 }
