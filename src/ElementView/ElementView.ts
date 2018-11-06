c99279f5f302a89ffb43d0408d98b1362d19bf76
Kiran Gadhave <kirangadhave2@gmail.com>
2018-11-06 14:28:45 -0700

    Setup events for addition and removal of selections

--- src/ElementView/ElementView.ts
+++ src/ElementView/ElementView.ts
@@ -1,7 +1,12 @@
+import { RenderRow } from "./../type_declarations/types";
 import { ViewBase } from "provenance_mvvm_framework";
 
 export class ElementView extends ViewBase {
   constructor(root: HTMLElement) {
     super(root);
+  }
+
+  update(data: RenderRow[]) {
+    console.log(data);
   }
 }
--- src/ElementView/ElementViewModel.ts
+++ src/ElementView/ElementViewModel.ts
@@ -11,6 +11,9 @@ export class ElementViewModel extends ViewModelBase {
     super(view, app);
     this.selectedSet = [];
 
+    this.App.on("add-selection", this.addSelection, this);
+    this.App.on("remove-selection", this.removeSelection, this);
+
     this.registerFunctions(
       "add-selection",
       (d: RenderRow) => {
@@ -21,11 +24,74 @@ export class ElementViewModel extends ViewModelBase {
 
     this.registerFunctions(
       "add-selection",
-      (d: RenderRow) => {
-        this.App.emit("remove-selection", d);
+      (idx: number) => {
+        this.App.emit("remove-selection", idx);
       },
       this,
       false
     );
+
+    this.registerFunctions(
+      "remove-selection",
+      (idx: number) => {
+        this.App.emit("remove-selection", idx);
+      },
+      this
+    );
+
+    this.registerFunctions(
+      "remove-selection",
+      (d: RenderRow) => {
+        this.App.emit("add-selection", d);
+      },
+      this,
+      false
+    );
+
+    this.comm.on("add-selection-trigger", (d: RenderRow) => {
+      let _do = {
+        func: (d: RenderRow) => {
+          this.App.emit("add-selection", d);
+        },
+        args: [d]
+      };
+      let _undo = {
+        func: (idx: number) => {
+          this.App.emit("remove-selection", idx);
+        },
+        args: [this.selectedSet.length]
+      };
+      this.apply.call(this, ["add-selection", _do, _undo]);
+    });
+
+    this.comm.on("remove-selection-trigger", (idx: number) => {
+      let _do = {
+        func: (idx: number) => {
+          this.App.emit("remove-selection", idx);
+        },
+        args: [idx]
+      };
+      let _undo = {
+        func: (d: RenderRow) => {
+          this.App.emit("add-selection", d);
+        },
+        args: [this.selectedSet[idx]]
+      };
+      this.apply.call(this, ["remove-selection", _do, _undo]);
+    });
+  }
+
+  addSelection(sel: RenderRow) {
+    this.selectedSet.push(sel);
+    this.update();
+  }
+
+  removeSelection(idx: number) {
+    this.selectedSet.splice(idx, 1);
+    this.update();
+  }
+
+  update() {
+    this.comm.emit("update", this.selectedSet);
   }
 }
