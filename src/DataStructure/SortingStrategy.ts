32baabc359db0a30fc2e8a990f17b0c7f923a473
Kiran Gadhave <kirangadhave2@gmail.com>
2018-11-27 16:57:00 -0700

    Refactored all sorting algorithms

--- src/DataStructure/SortingStrategy.ts
+++ src/DataStructure/SortingStrategy.ts
@@ -16,30 +16,6 @@ SortStrategy[SortBy.SET] = sortBySet;
 export default SortStrategy;
 
 //  Sorting function definitions
-
-function sortByCardinality(data: RenderRow[]): RenderRow[] {
-  let groups = data.reduce((p, c, i) => {
-    if (c.data.type === RowType.GROUP) p.push(i);
-    return p;
-  }, []);
-  let rr: Array<RenderRow> = [];
-  if (groups.length === 0) {
-    return data.sort((d1, d2) => {
-      return d2.data.setSize - d1.data.setSize;
-    });
-  }
-  groups.forEach((g, idx) => {
-    rr.push(data[g]);
-    let els = data.slice(g + 1, groups[idx + 1]);
-    els = els.sort((d1, d2) => {
-      return d2.data.setSize - d1.data.setSize;
-    });
-    rr = rr.concat(els);
-  });
-
-  return rr;
-}
-
 function sortBySet(data: RenderRow[], setId: number): RenderRow[] {
   let a = data.sort((d1, d2) => {
     return (
@@ -53,21 +29,40 @@ function sortBySet(data: RenderRow[], setId: number): RenderRow[] {
 function sortByDegree(data: RenderRow[], setId: number): RenderRow[] {
   return data;
 }
+
 function sortByDeviation(data: RenderRow[], setId: number): RenderRow[] {
-  data = data.sort((a, b) => {
-    return a.data.disproportionality - b.data.disproportionality;
-  });
+  data.sort((a, b) => b.data.disproportionality - a.data.disproportionality);
   if (data.filter(_ => _.data.type === RowType.GROUP).length > 0) {
     data.forEach(row => {
       let group = row.data as Group;
-      group.visibleSets = group.visibleSets.sort(
-        (a, b) => a.disproportionality - b.disproportionality
+      group.visibleSets.sort(
+        (a, b) => b.disproportionality - a.disproportionality
       );
       if (group.nestedGroups.length > 0) {
+        group.nestedGroups.sort(
+          (a, b) => b.disproportionality - a.disproportionality
+        );
         group.nestedGroups.forEach(ng => {
-          ng.visibleSets = ng.visibleSets.sort(
-            (a, b) => a.disproportionality - b.disproportionality
+          ng.visibleSets.sort(
+            (a, b) => b.disproportionality - a.disproportionality
           );
+        });
+      }
+    });
+  }
+  return data;
+}
+
+function sortByCardinality(data: RenderRow[]): RenderRow[] {
+  data.sort((a, b) => b.data.setSize - a.data.setSize);
+  if (data.filter(_ => _.data.type === RowType.GROUP).length > 0) {
+    data.forEach(row => {
+      let group = row.data as Group;
+      group.visibleSets.sort((a, b) => b.setSize - a.setSize);
+      if (group.nestedGroups.length > 0) {
+        group.nestedGroups.sort((a, b) => b.setSize - a.setSize);
+        group.nestedGroups.forEach(ng => {
+          ng.visibleSets.sort((a, b) => b.setSize - a.setSize);
         });
       }
     });
