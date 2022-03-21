272ca8b99a6d2d0afb156c4e5b2a4c91451d0c33
Kiran Gadhave <kirangadhave2@gmail.com>
2022-03-21 16:03:08 -0600

    Changed VUE prefix to REACT

--- packages/app/src/atoms/authAtoms.ts
+++ packages/app/src/atoms/authAtoms.ts
@@ -2,14 +2,14 @@ import OauthClient from '@girder/oauth-client';
 import { multinetApi } from 'multinet';
 
 export const host: string =
-  process.env.VUE_APP_MULTINET_HOST || 'http://localhost:8000';
+  process.env.REACT_APP_MULTINET_HOST || 'http://localhost:8000';
 
 export const oauthApiRoot: string =
-  process.env.VUE_APP_OAUTH_API_ROOT || `${host}/oauth/`;
+  process.env.REACT_APP_OAUTH_API_ROOT || `${host}/oauth/`;
 
 const login_url = `${host}/oauth/`;
 const client_id =
-  process.env.VUE_APP_OAUTH_CLIENT_ID ||
+  process.env.REACT_APP_OAUTH_CLIENT_ID ||
   '7K4fAnTtGGjCKEI6RpXxCFAM5nHG9jhTmBGsSw5x';
 
 console.log({ host, oauthApiRoot, login_url, client_id });
