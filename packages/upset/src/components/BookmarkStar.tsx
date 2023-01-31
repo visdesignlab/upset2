3fd0a576dd06d361a9e52fdf92b8654c4adc7861
Jake Wagoner <jakewagoneredu@gmail.com>
2023-01-31 15:51:20 -0700

    style: 💄 Made star clickable to select set

--- packages/upset/src/components/BookmarkStar.tsx
+++ packages/upset/src/components/BookmarkStar.tsx
@@ -1,10 +1,11 @@
 import { Row } from '@visdesignlab/upset2-core';
 import { FC } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import StarIcon from '@mui/icons-material/Star'
 import { dimensionsSelector } from '../atoms/dimensionsAtom';
 import translate from '../utils/transform';
-import { bookmarkedColorPalette } from '../atoms/config/currentIntersectionAtom';
+import { bookmarkedColorPalette, currentIntersectionAtom } from '../atoms/config/currentIntersectionAtom';
+import { css } from '@emotion/react';
 
 type Props = {
     row: Row;
@@ -13,15 +14,18 @@ type Props = {
 export const BookmarkStar: FC<Props> = ({ row }) => {
     const dimensions = useRecoilValue(dimensionsSelector);
     const colorPallete = useRecoilValue(bookmarkedColorPalette);
+    const setCurrentIntersectionAtom = useSetRecoilState(currentIntersectionAtom);
 
-    return (<g transform={
-        translate(
+    return (<g 
+        onClick={() => row && (setCurrentIntersectionAtom(row))}
+        transform={translate(
             dimensions.matrixColumn.width + 
             dimensions.bookmarkStar.gap +
             dimensions.bookmarkStar.gap / 2 +
             dimensions.cardinality.width + 
             dimensions.bookmarkStar.gap,
-            0)}>
+            0)}
+            css={css`cursor:pointer;`}>
                 <StarIcon height={dimensions.body.rowHeight} width={dimensions.set.width} fontSize={'1em' as any} sx={{ color: colorPallete[row.id] }}/>
             </g>)
 }
