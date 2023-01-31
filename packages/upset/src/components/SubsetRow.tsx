44a94e826a18c1eacae8fcee8ca29a5d17e306e6
Jake Wagoner <jakewagoneredu@gmail.com>
2023-01-31 11:41:07 -0700

    style: 💄 Add bookmark icon on set rows

--- packages/upset/src/components/SubsetRow.tsx
+++ packages/upset/src/components/SubsetRow.tsx
@@ -7,10 +7,11 @@ import { AttributeBars } from './AttributeBars';
 import { CardinalityBar } from './CardinalityBar';
 import { DeviationBar } from './DeviationBar';
 import { Matrix } from './Matrix';
-import { currentIntersectionAtom } from '../atoms/config/currentIntersectionAtom';
+import { bookmarkedIntersectionSelector, currentIntersectionAtom, bookmarkedColorPalette } from '../atoms/config/currentIntersectionAtom';
 import { dimensionsSelector } from '../atoms/dimensionsAtom';
-import { highlightBackground } from '../utils/styles';
-import { css } from '@emotion/react';
+import { highlightBackground, defaultBackground } from '../utils/styles';
+import StarIcon from '@mui/icons-material/Star';
+import translate from '../utils/transform'
 
 type Props = {
   subset: Subset;
@@ -20,10 +21,15 @@ export const SubsetRow: FC<Props> = ({ subset }) => {
   const visibleSets = useRecoilValue(visibleSetSelector);
   const currentIntersection = useRecoilValue(currentIntersectionAtom);
   const dimensions = useRecoilValue(dimensionsSelector);
+  const bookmarkedIntersections = useRecoilValue(bookmarkedIntersectionSelector);
+  const colorPallete = useRecoilValue(bookmarkedColorPalette);
 
   return (
     <>
-      <rect height={dimensions.body.rowHeight} width={dimensions.body.rowWidth} css={currentIntersection === subset ? highlightBackground : css`fill:#ffffff`} rx="5" ry="10"></rect>
+      <rect height={dimensions.body.rowHeight} width={dimensions.body.rowWidth} css={currentIntersection === subset ? highlightBackground : defaultBackground} rx="5" ry="10"></rect>
+      {bookmarkedIntersections.includes(subset.id) &&
+        <g transform={translate(dimensions.set.label.height - dimensions.set.width - dimensions.gap,0)}><StarIcon height={dimensions.body.rowHeight} width={dimensions.set.width} fontSize={'1em' as any} sx={{ color: colorPallete[subset.id] }}/></g>
+      }
       <Matrix sets={visibleSets} subset={subset} />
       <CardinalityBar size={subset.size} row={subset} />
       <DeviationBar deviation={subset.deviation} />
--- packages/upset/src/utils/styles.ts
+++ packages/upset/src/utils/styles.ts
@@ -3,6 +3,14 @@ import { css } from '@emotion/react';
 /** @jsxImportSource @emotion/react */
 export const highlightBackground = css`
   fill: #fed9a6;
+  stroke: #feca82;
+  stroke-width: 2;
+  stroke-opacity: 0.8;
+`;
+
+// transparent default
+export const defaultBackground = css`
+  fill-opacity: 0.0;
 `;
 
 export const queryColorPalette = [
