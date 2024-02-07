b50f7c78750d84d16cd3aeaaa2507b2e662b13db
Jake Wagoner <jakewagoneredu@gmail.com>
2024-02-07 11:15:27 -0700

    Add yarn build step to playwright webServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'yarn build && yarn dev',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     stdout: 'ignore',
