96f16d96288d35850240c49d832ecee873b4b5d5
Nate Lanza <natelanzadevelopment@gmail.com>
2024-06-11 09:23:59 -0600

    *BUGGY* brush handler for element selection (still includes print statements & doesn't apply saved selections from state)

--- packages/upset/src/components/ElementView/ElementVisualization.tsx
+++ packages/upset/src/components/ElementView/ElementVisualization.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@mui/material';
 import { Box } from '@mui/system';
-import { useState } from 'react';
-import { SignalListener, VegaLite } from 'react-vega';
+import { useContext, useRef, useState } from 'react';
+import { SignalListener, VegaLite, View } from 'react-vega';
 import { useRecoilValue } from 'recoil';
 
 import { bookmarkedIntersectionSelector } from '../../atoms/config/currentIntersectionAtom';
@@ -9,6 +9,9 @@ import { histogramSelector, scatterplotsSelector } from '../../atoms/config/plot
 import { elementItemMapSelector } from '../../atoms/elementsSelectors';
 import { AddPlotDialog } from './AddPlotDialog';
 import { generateVega } from './generatePlotSpec';
+import { ProvenanceContext } from '../Root';
+import { ElementSelection } from '@visdesignlab/upset2-core';
+import { upsetConfigAtom } from '../../atoms/config/upsetConfigAtoms';
 
 export const ElementVisualization = () => {
   const [openAddPlot, setOpenAddPlot] = useState(false);
@@ -16,30 +19,49 @@ export const ElementVisualization = () => {
   const histograms = useRecoilValue(histogramSelector);
   const bookmarked = useRecoilValue(bookmarkedIntersectionSelector);
   const items = useRecoilValue(elementItemMapSelector(bookmarked.map((b) => b.id)));
-  const [brushing, setBrushing] = useState(false);
-
+  
+  const { actions } = useContext(ProvenanceContext);
+  const config = useRecoilValue(upsetConfigAtom);
+  const [elementSelection, setElementSelection] = useState<ElementSelection>(config.elementSelection);
+  const timeout = useRef<number | null>(null);
+  
   const onClose = () => setOpenAddPlot(false);
 
   /**
    * Saves brush bounds to state when the interactive brush is used.
-   * @param {string} _ Name of the signal: "brush"
+   * @param {string}  _     Name of the signal: "brush"
    * @param {unknown} value Should be an object mapping the names of the attributes being brushed over
-   *                        to an array of the bounds of the brush, but apparently Vega makes no guarantees.
+   *  to an array of the bounds of the brush, but Vega's output format can change if the spec changes.
    */
   const brushHandler: SignalListener = (_: string, value: unknown) => {
-    console.log(value);
-  };
+    console.log("brushHandler", value);
+    if ( // Validates that the signal is in the expected format; allows us to convert unknown to ElementSelection
+      value === undefined 
+      || typeof value !== "object"
+      || (Object.keys(value as Object).length > 0 
+        && !Object.values(value as Object).some((v) => 
+          !Array.isArray(v) 
+          || !(v.length === 2) 
+          || typeof v[0] !== "number" 
+          || typeof v[1] !== "number")
+      )
+    ) return;
 
-  console.log(JSON.stringify(generateVega(scatterplots, histograms)));
+    setElementSelection(value as ElementSelection);
+
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+    timeout.current = setTimeout(() => {
+      actions.setElementSelection(value as ElementSelection);
+    }, 2000); // Delay for 2 seconds before saving the selection to the provenance state
+  };
 
   return (
     <Box>
       <Button onClick={() => setOpenAddPlot(true)}>Add Plot</Button>
       <AddPlotDialog open={openAddPlot} onClose={onClose} />
-      <Box
-        onMouseDown={() => setBrushing(true)}
-        onMouseUp={() => setBrushing(false)}
-      >
+      <Box sx={{ overflowX: 'auto' }}>
         {(scatterplots.length > 0 || histograms.length > 0) && (
           <VegaLite
             spec={generateVega(scatterplots, histograms)}
@@ -49,6 +71,16 @@ export const ElementVisualization = () => {
             actions={false}
             signalListeners={{
               "brush": brushHandler,
+            }}
+            onNewView={(view: View) => {
+              // elementSelection should default to config.elementSelection (checked in the useState call above)
+              // but it's actually undefined here, so this is my fix :/
+              const defaultSelection = elementSelection ?? config.elementSelection;
+              if (defaultSelection) {
+                view.signal('brush', defaultSelection);
+                console.log(view.signal('brush'));
+                view.runAsync().then((v: View) => {console.log(v.signal('brush'))});
+              }
             }}
           />
         )}
