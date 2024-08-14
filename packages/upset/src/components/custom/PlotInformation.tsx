f375e5ec9a548f4bca3abc0d7e989b09fe26d7d4
Nate Lanza <natelanzadevelopment@gmail.com>
2024-08-14 13:39:48 -0600

    Implement eslint suggestions for PlotInformation and remove hover effect

--- packages/upset/src/components/custom/PlotInformation.tsx
+++ packages/upset/src/components/custom/PlotInformation.tsx
@@ -6,9 +6,11 @@ import {
   Typography,
 } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
-import { plotInformationSelector } from '../../atoms/config/plotInformationAtom';
 import { useRecoilValue } from 'recoil';
-import { useContext, useState } from 'react';
+import {
+  useContext, useState, JSX, useCallback,
+} from 'react';
+import { plotInformationSelector } from '../../atoms/config/plotInformationAtom';
 import { ProvenanceContext } from '../Root';
 
 /**
@@ -35,12 +37,16 @@ type Props = {
  * Uses up to 5 tab indices, starting from @param tabIndex
  * @param Props @see @type Props
  */
-export const PlotInformation = ({onSave, divider, tabIndex}: Props) => {
+export const PlotInformation = ({ onSave, divider, tabIndex }: Props) => {
   /**
-   * Width of the titles for all the fields in %. 
+   * Width of the titles for all the fields in %.
    * Field entry boxes will occupy the rest of the space
    */
   const fieldTitleWidth = 30;
+
+  /**
+   * Constants
+   */
 
   const plotInfoItem = {
     display: 'flex',
@@ -49,36 +55,41 @@ export const PlotInformation = ({onSave, divider, tabIndex}: Props) => {
     margin: '0.25em 0',
     minHeight: '4em',
   };
-  
   const plotInfoTitle = {
     fontSize: '1em',
     fontWeight: 'inherit',
     color: 'GrayText',
-    width: fieldTitleWidth + '%',
+    width: `${fieldTitleWidth}%`,
   };
-
   const placeholderText = {
     description: 'description of this dataset, eg: "movie genres and ratings"',
     sets: 'name for the sets in this data, eg: "movie genres"',
     items: 'name for the items in this data, eg: "movies"',
   };
 
+  /**
+   * State
+   */
+
   const plotInformationState = useRecoilValue(plotInformationSelector);
   const [plotInformation, setPlotInformation] = useState(plotInformationState);
   const { actions } = useContext(ProvenanceContext);
-
   const [editing, setEditing] = useState(false);
+
+  /**
+   * Functions
+   */
 
   /**
    * Generates plot information string
    * @returns string Plot information description
    */
-  const generatePlotInformationText: () => string = () => {
+  const generatePlotInformationText: () => string = useCallback(() => {
     // return default string if there are no values filled in
     if (Object.values(plotInformation).filter((a) => a.length > 0).length === 0) {
       return `This UpSet plot shows ${placeholderText.description}. The sets are ${placeholderText.sets}. The items are ${placeholderText.items}`;
     }
-    
+
     let str: string = '';
     if (plotInformation.description !== '') {
       str += `This UpSet plot shows ${plotInformation.description}. `;
@@ -89,23 +100,22 @@ export const PlotInformation = ({onSave, divider, tabIndex}: Props) => {
     if (plotInformation.items !== '') {
       str += `The items are ${plotInformation.items}.`;
     }
-    
+
     return str;
-  };
+  }, [plotInformation, placeholderText]);
 
   /**
    * Commits changes to the plot information if the new state is different from the old state.
-   * If a commit occurs, records a trrack action. 
+   * If a commit occurs, records a trrack action.
    */
-  const commitEdits = () => { 
+  const commitEdits = () => {
     if ( // Need to manually check for changes since the state is an object
       plotInformation.caption !== plotInformationState.caption
       || plotInformation.title !== plotInformationState.title
       || plotInformation.description !== plotInformationState.description
       || plotInformation.sets !== plotInformationState.sets
       || plotInformation.items !== plotInformationState.items
-    )
-      actions.setPlotInformation(plotInformation);
+    ) actions.setPlotInformation(plotInformation);
     if (onSave) onSave();
     setEditing(false);
   };
@@ -115,71 +125,73 @@ export const PlotInformation = ({onSave, divider, tabIndex}: Props) => {
       <Box
         tabIndex={tabIndex}
         onClick={() => setEditing(true)}
-        sx={{ 
+        sx={{
           cursor: 'pointer',
           border: '2px solid white', // Prevent resize on hover
           padding: '.2em',
-          '&:hover': {
-            border: '2px inset #ddd', 
-          }
         }}
       >
-        <div style={{height: '1.6em'}}>
+        <div style={{ height: '1.6em' }}>
           <Typography variant="h2" fontSize="1.2em" fontWeight="inherit" height="1.4em" padding="0">
-            {plotInformation.title ?? "[Title]"}
+            {plotInformation.title ?? '[Title]'}
           </Typography>
-          <Button 
-            aria-label='Plot Information Editor' 
-            style={{float: 'right', position: 'relative', bottom: '40px'}} 
+          <Button
+            aria-label="Plot Information Editor"
+            style={{ float: 'right', position: 'relative', bottom: '40px' }}
             tabIndex={tabIndex + 1}
           >
-            <Icon style={{overflow: 'visible'}}>
+            <Icon style={{ overflow: 'visible' }}>
               <EditIcon />
             </Icon>
           </Button>
         </div>
         {divider}
-        <Typography>{plotInformation.caption ?? "[Caption]"}</Typography>
+        <Typography>{plotInformation.caption ?? '[Caption]'}</Typography>
         <br />
         <Typography>{generatePlotInformationText()}</Typography>
       </Box>
     ) : (
       <Box tabIndex={tabIndex}>
-        <Button 
-          tabIndex={tabIndex + 6} 
-          color="primary" 
-          style={{float: 'right'}} 
+        <Button
+          tabIndex={tabIndex + 6}
+          color="primary"
+          style={{ float: 'right' }}
           onClick={commitEdits}
-        >Save</Button>
+        >
+          Save
+        </Button>
         <Box>
           <TextField
-            tabIndex={tabIndex + 1} 
-            fullWidth 
-            variant='standard'
-            style={{marginBottom: "5px"}}
+            tabIndex={tabIndex + 1}
+            fullWidth
+            variant="standard"
+            style={{ marginBottom: '5px' }}
             value={plotInformation.title}
             onChange={(e) => setPlotInformation({ ...plotInformation, title: e.target.value })}
-            placeholder='Title'
+            placeholder="Title"
             inputProps={{
               disableUnderline: true,
               style: {
-                padding: "1px", 
-                height: "1.4em", 
-                fontSize: "1.2em", 
-                fontWeight: "inherit",
-                border: "none",
-            }}}/>
-          <TextField 
+                padding: '1px',
+                height: '1.4em',
+                fontSize: '1.2em',
+                fontWeight: 'inherit',
+                border: 'none',
+              },
+            }}
+          />
+          <TextField
             tabIndex={tabIndex + 2}
-            fullWidth multiline
+            fullWidth
+            multiline
             inputProps={{
               rows: 3,
               // We need to override the default overflow prop (hidden), then still deny x scrolling
-              style: {height: "4em", overflow: "auto", overflowX: "hidden"}
+              style: { height: '4em', overflow: 'auto', overflowX: 'hidden' },
             }}
             value={plotInformation.caption}
             onChange={(e) => setPlotInformation({ ...plotInformation, caption: e.target.value })}
-            placeholder='Caption'
+            placeholder="Caption"
           />
         </Box>
         <Box>
@@ -190,7 +202,7 @@ export const PlotInformation = ({onSave, divider, tabIndex}: Props) => {
             <TextField
               tabIndex={tabIndex + 3}
               onChange={(e) => setPlotInformation({ ...plotInformation, description: e.target.value })}
-              sx={{ width: 100 - fieldTitleWidth + '%' }}
+              sx={{ width: `${100 - fieldTitleWidth}%` }}
               multiline
               InputLabelProps={{ shrink: true }}
               value={plotInformation.description}
@@ -208,7 +220,7 @@ export const PlotInformation = ({onSave, divider, tabIndex}: Props) => {
             <TextField
               tabIndex={tabIndex + 4}
               onChange={(e) => setPlotInformation({ ...plotInformation, sets: e.target.value })}
-              sx={{ width: 100 - fieldTitleWidth + '%' }}
+              sx={{ width: `${100 - fieldTitleWidth}%` }}
               multiline
               InputLabelProps={{ shrink: true }}
               value={plotInformation.sets}
@@ -226,7 +238,7 @@ export const PlotInformation = ({onSave, divider, tabIndex}: Props) => {
             <TextField
               tabIndex={tabIndex + 5}
               onChange={(e) => setPlotInformation({ ...plotInformation, items: e.target.value })}
-              sx={{ width: 100 - fieldTitleWidth + '%' }}
+              sx={{ width: `${100 - fieldTitleWidth}%` }}
               multiline
               InputLabelProps={{ shrink: true }}
               value={plotInformation.items}
@@ -239,5 +251,5 @@ export const PlotInformation = ({onSave, divider, tabIndex}: Props) => {
         <Typography>{generatePlotInformationText()}</Typography>
       </Box>
     )
-  )
-}
+  );
+};
