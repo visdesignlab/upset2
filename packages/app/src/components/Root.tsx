0b4fd4c8f3a46ea883c077c1d76692b106cc755f
Nate Lanza <natelanzadevelopment@gmail.com>
2024-12-15 19:33:40 -0700

    Remove useless useEffect

--- packages/app/src/components/Root.tsx
+++ packages/app/src/components/Root.tsx
@@ -2,7 +2,7 @@ import { UpsetActions, UpsetProvenance } from '@visdesignlab/upset2-react';
 import { UpsetConfig } from '@visdesignlab/upset2-core';
 import { Box, css } from '@mui/material';
 import {
-  useRef, useState, useEffect, useMemo,
+  useMemo,
 } from 'react';
 import { Body } from './Body';
 import Header from './Header';
@@ -19,24 +19,12 @@ type Props = {
 export const Root = ({
   provenance, actions, data, config,
 }: Props) => {
-  const headerDiv = useRef<HTMLDivElement>(null);
-  const [headerHeight, setHeaderHeight] = useState(-1);
-
   const AppCss = useMemo(() => css`
         overflow: ${data === null ? 'auto' : 'hidden'};
         height: 100vh;
         display: grid;
         grid-template-rows: min-content auto;
       `, [data]);
-
-  useEffect(() => {
-    const { current } = headerDiv;
-    if (!current) return;
-
-    if (headerHeight > 0) return;
-
-    setHeaderHeight(current.clientHeight);
-  }, [headerHeight, headerDiv]);
 
   return (
     <div css={AppCss}>
@@ -45,7 +33,6 @@ export const Root = ({
           zIndex: (theme) => theme.zIndex.drawer + 1,
           position: 'relative',
         }}
-        ref={headerDiv}
       >
         <Header data={data} />
       </Box>
