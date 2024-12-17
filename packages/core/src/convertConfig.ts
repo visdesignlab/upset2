ec2d6ae1f255ceaa33b2290a6a1bad240c4ae84f
NateLanza <natelanzadevelopment@gmail.com>
2024-12-17 13:14:45 -0700

    Add config fields for intersection size lables, set size labels, and hidden sets

--- packages/core/src/convertConfig.ts
+++ packages/core/src/convertConfig.ts
@@ -1,6 +1,9 @@
 import {
   AggregateBy, Bookmark, NumericalBookmark, Column, ColumnName, Histogram, PlotInformation, Row, Scatterplot,
   SortByOrder, SortVisibleBy, UpsetConfig,
+  AttributePlots,
+  ElementSelection,
+  AltText,
 } from './types';
 import { isUpsetConfig } from './typecheck';
 
@@ -22,6 +25,39 @@ import { isUpsetConfig } from './typecheck';
  * 6. Update isUpsetConfig (in typecheck.ts) to reflect your changes to UpsetConfig.
  * 7. Bump the version number in the UpsetConfig type, all package.json files, the README, and defaultConfig.ts.
  */
+
+type Version0_1_0 = {
+  plotInformation: PlotInformation;
+    horizontal: boolean;
+    firstAggregateBy: AggregateBy;
+    firstOverlapDegree: number;
+    secondAggregateBy: AggregateBy;
+    secondOverlapDegree: number;
+    sortVisibleBy: SortVisibleBy;
+    sortBy: string;
+    sortByOrder: SortByOrder;
+    filters: {
+      maxVisible: number;
+      minVisible: number;
+      hideEmpty: boolean;
+      hideNoSet: boolean;
+    };
+    visibleSets: ColumnName[];
+    visibleAttributes: ColumnName[];
+    attributePlots: AttributePlots;
+    bookmarks: Bookmark[];
+    collapsed: string[];
+    plots: {
+      scatterplots: Scatterplot[];
+      histograms: Histogram[];
+    };
+    allSets: Column[];
+    selected: Row | null;
+    elementSelection: ElementSelection | null;
+    version: '0.1.0';
+    useUserAlt: boolean;
+    userAltText: AltText | null;
+}
 
 /**
  * Config type before versioning was implemented.
@@ -56,27 +92,39 @@ type PreVersionConfig = {
 };
 
 /**
+ * Converts a 0.1.0 config to the current version.
+ * @param config The config to convert.
+ * @returns The converted config.
+ */
+// eslint-disable-next-line camelcase
+function convert0_1_0(config: Version0_1_0): UpsetConfig {
+  return {
+    ...config, version: '0.1.1', intersectionSizeLabels: true, setSizeLabels: true, showHiddenSets: true,
+  };
+}
+
+/**
  * Converts a pre-versioned config to the current version.
  * @param config The config to convert.
  * @returns The converted config.
  */
-function preVersionConversion(config: PreVersionConfig): UpsetConfig {
+function preVersionConversion(config: PreVersionConfig): Version0_1_0 {
   // TS won't allow a conversion directly to UpsetConfig, so we have to cast it to unknown first.
   // This is necessary to add and remove properties from the object.
-  (config as unknown as UpsetConfig).version = '0.1.0';
-  (config as unknown as UpsetConfig).elementSelection = null;
-  (config as unknown as UpsetConfig).bookmarks = config.bookmarkedIntersections;
-  (config as unknown as UpsetConfig).useUserAlt = false;
-  (config as unknown as UpsetConfig).userAltText = null;
-  (config as unknown as UpsetConfig).attributePlots = {};
+  (config as unknown as Version0_1_0).version = '0.1.0';
+  (config as unknown as Version0_1_0).elementSelection = null;
+  (config as unknown as Version0_1_0).bookmarks = config.bookmarkedIntersections;
+  (config as unknown as Version0_1_0).useUserAlt = false;
+  (config as unknown as Version0_1_0).userAltText = null;
+  (config as unknown as Version0_1_0).attributePlots = {};
   // Any cast required because bookmarkedIntersections isn't optional in PreversionConfig
   delete (config as any).bookmarkedIntersections;
 
-  (config as unknown as UpsetConfig).bookmarks.forEach((bookmark) => {
+  (config as unknown as Version0_1_0).bookmarks.forEach((bookmark) => {
     bookmark.type = 'intersection';
   });
 
-  return config as unknown as UpsetConfig;
+  return config as unknown as Version0_1_0;
 }
 
 /**
@@ -93,15 +141,16 @@ export function convertConfig(config: unknown): UpsetConfig {
   if (!Object.hasOwn(config, 'version')) preVersionConversion(config as PreVersionConfig);
 
   /* eslint-disable no-void */
+  /* eslint-disable no-fallthrough */
   // Switch case is designed to fallthrough to the next version's conversion function
   // so that all versions are converted cumulatively.
   switch ((config as {version: string}).version) {
     case '0.1.0':
-      void 0; // This will be replaced by the next version's conversion function
-      // falls through
+      convert0_1_0(config as Version0_1_0);
     default:
       void 0;
   }
+  /* eslint-enable no-fallthrough */
   /* eslint-enable no-void */
 
   if (!isUpsetConfig(config)) {
