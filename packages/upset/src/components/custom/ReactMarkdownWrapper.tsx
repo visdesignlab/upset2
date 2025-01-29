bc317b615f15bf9d688e28aaf5c88caf81561f79
NateLanza <natelanzadevelopment@gmail.com>
2025-01-28 18:37:05 -0700

    Sidebar text styling updates

--- packages/upset/src/components/custom/ReactMarkdownWrapper.tsx
+++ packages/upset/src/components/custom/ReactMarkdownWrapper.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material';
 import Markdown from 'react-markdown';
 
 // Wrapper for the Markdown component to parse and render the alt-txt json
@@ -5,6 +6,8 @@ export default function ReactMarkdownWrapper({ text }: { text: string }) {
   const components = {
     // eslint-disable-next-line react/no-unstable-nested-components, jsx-a11y/heading-has-content
     h1: ({ node, ...props }: { node: any }) => <h3 id={node.children[0].value} {...props} />,
+    // eslint-disable-next-line react/no-unstable-nested-components, jsx-a11y/heading-has-content
+    p: ({ node, ...props }: { node: any }) => <Typography {...props} />,
   };
 
   return (
