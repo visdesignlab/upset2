dc8b711d79490f62c7ca0513fcaacc54371d0d41
Jake Wagoner <jakewagoneredu@gmail.com>
2025-03-04 10:04:36 -0700

    Add docstring to setQuerySizeSelector

--- packages/upset/src/atoms/setQuerySizeSelector.ts
+++ packages/upset/src/atoms/setQuerySizeSelector.ts
@@ -1,6 +1,11 @@
 import { selector } from 'recoil';
 import { flattenedRowsSelector } from './renderRowsAtom';
 
+/**
+ * Selector to calculate the total size of all rows present within a set query.
+ *
+ * @returns {number} The total size of all rows in the set query.
+ */
 export const setQuerySizeSelector = selector<number>({
   key: 'setQuerySizeSelector',
   get: ({ get }) => {
