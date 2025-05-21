c72ba1e58001a67229ed5b0ad7e6271e35048bbc
NateLanza <natelanzadevelopment+github@gmail.com>
2025-05-21 12:39:35 -0600

    Prettier working with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,46 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import stylisticJS from "@stylistic/eslint-plugin";
-import importPlugin from "eslint-plugin-import";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import stylisticJS from '@stylistic/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   importPlugin.flatConfigs.recommended,
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  eslintPluginPrettierRecommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
   {
     plugins: {
-      "react": pluginReact,
-      "@stylistic": stylisticJS,
-      "eslint-plugin-react": pluginReact,
+      react: pluginReact,
+      '@stylistic': stylisticJS,
+      'eslint-plugin-react': pluginReact,
     },
     rules: {
-      'react/jsx-filename-extension': [
-        2,
-        { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+          singleQuote: true,
+          trailingComma: 'all',
+          printWidth: 100,
+          semi: true,
+          tabWidth: 2,
+          useTabs: false,
+        },
       ],
+      'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
       'import/prefer-default-export': 'off',
       'react/function-component-definition': 'off',
       'no-plusplus': ['warn', { allowForLoopAfterthoughts: true }],
@@ -31,7 +49,7 @@ export default defineConfig([
       'react/jsx-props-no-spreading': 'off',
       'react/require-default-props': 'off',
       'react/react-in-jsx-scope': 'off',
-      "react/no-unknown-property": ['error', { ignore: ['css'] }],
+      'react/no-unknown-property': ['error', { ignore: ['css'] }],
       'operator-linebreak': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn', // or "error"
@@ -52,10 +70,7 @@ export default defineConfig([
       'no-trailing-spaces': 'error',
       'import/named': 'off',
       'import/no-unresolved': 'off',
-      'no-multiple-empty-lines': [
-        'error',
-        { max: 1, maxEOF: 0, maxBOF: 0 },
-      ],
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0 }],
     },
   },
 ]);
