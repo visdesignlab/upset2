3333cb059852d44ae350dd6aa300bf4b1967d0f3
NateLanza <natelanzadevelopment+github@gmail.com>
2025-05-21 12:12:53 -0600

    Heinous fix for eslint... I don't even know why it broke in the first place

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import stylisticJS from "@stylistic/eslint-plugin";
+import importPlugin from "eslint-plugin-import";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  importPlugin.flatConfigs.recommended,
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  {
+    plugins: {
+      "react": pluginReact,
+      "@stylistic": stylisticJS,
+      "eslint-plugin-react": pluginReact,
+    },
+    rules: {
+      'react/jsx-filename-extension': [
+        2,
+        { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+      ],
+      'import/prefer-default-export': 'off',
+      'react/function-component-definition': 'off',
+      'no-plusplus': ['warn', { allowForLoopAfterthoughts: true }],
+      'dot-notation': 'off',
+      'import/extensions': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/require-default-props': 'off',
+      'react/react-in-jsx-scope': 'off',
+      "react/no-unknown-property": ['error', { ignore: ['css'] }],
+      'operator-linebreak': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn', // or "error"
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'max-len': 'off',
+      'no-param-reassign': 'off',
+      'import/no-cycle': 'off',
+      'no-underscore-dangle': 'off',
+      'no-nested-ternary': 'off',
+      'jsx-a11y/tabindex-no-positive': 'off',
+      'no-bitwise': 'warn',
+      'no-restricted-syntax': 'warn',
+      'no-trailing-spaces': 'error',
+      'import/named': 'off',
+      'import/no-unresolved': 'off',
+      'no-multiple-empty-lines': [
+        'error',
+        { max: 1, maxEOF: 0, maxBOF: 0 },
+      ],
+    },
+  },
+]);
