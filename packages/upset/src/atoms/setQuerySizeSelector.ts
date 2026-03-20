6bf713c33030eaf6606b4b5d7807335e1e383c20
Jack Wilburn <jackwilburn@tutanota.com>
2026-03-20 13:18:23 -0600

    Fix set query size totals

--- packages/upset/src/atoms/setQuerySizeSelector.ts
+++ packages/upset/src/atoms/setQuerySizeSelector.ts
@@ -1,5 +1,27 @@
+import {
+  getBelongingSetsFromSetMembership,
+  isPopulatedSetQuery,
+  isRowAggregate,
+  Row,
+  SetQueryMembership,
+} from '@visdesignlab/upset2-core';
 import { selector } from 'recoil';
+import { setQuerySelector } from './config/queryBySetsAtoms';
 import { flattenedRowsSelector } from './renderRowsAtom';
+
+const rowMatchesSetQuery = (row: Row, membership: SetQueryMembership) => {
+  const belongingSets = getBelongingSetsFromSetMembership(row.setMembership);
+
+  return Object.entries(membership).every(([set, status]) => {
+    if (status === 'Yes') {
+      return belongingSets.includes(set);
+    }
+    if (status === 'No') {
+      return !belongingSets.includes(set);
+    }
+    return true;
+  });
+};
 
 /**
  * Selector to calculate the total size of all rows present within a set query.
@@ -9,11 +31,17 @@ import { flattenedRowsSelector } from './renderRowsAtom';
 export const setQuerySizeSelector = selector<number>({
   key: 'setQuerySizeSelector',
   get: ({ get }) => {
+    const setQuery = get(setQuerySelector);
+    if (setQuery === null || !isPopulatedSetQuery(setQuery)) return 0;
+    const { query } = setQuery;
+
     const rows = get(flattenedRowsSelector);
-    let total = 0;
-    rows.forEach((rr) => {
-      total += rr.row.size;
-    });
-    return total;
+    return rows.reduce((total, { row }) => {
+      if (isRowAggregate(row) || !rowMatchesSetQuery(row, query)) {
+        return total;
+      }
+
+      return total + row.size;
+    }, 0);
   },
 });
